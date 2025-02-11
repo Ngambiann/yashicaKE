2dcb2f556c876066ed2634debf63e7136343e31a
ann-ngambi <ngambiann08@gmail.com>
2025-02-11 04:27:19 +0300

    comments

--- yashicaKE/src/routes/index.js
+++ yashicaKE/src/routes/index.js
@@ -2,7 +2,7 @@ import {createRouter,createWebHistory} from 'vue-router'
 
 import Homepage from "./../components/Homepage.vue"
 import Cameras from "./../components/Cameras.vue"
-import Cart from "./../components/Cart.vue"
+//import Cart from "./../components/Cart.vue"
 import Contactus from "./../components/Contactus.vue"
 import Aboutus from "./../components/Aboutus.vue"
 
